f30b8f50053ef77bc53dc93ed2f05acab178b704
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2023-10-09 20:25:30 +0200

    Wstępne rozwiązanie zadania z Informatyki

--- CesarEncryptionAttack.java
+++ CesarEncryptionAttack.java
@@ -1,91 +1,77 @@
 package sb;
 
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CesarEncryptionAttack {
-    // alphabet
-    // (32) !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz (122)
-    public CesarEncryptionAttack() {
-
-    }
-
     public static void main(String[] args) {
-        String input = CesarEncryptionAttack.inputFromUser();
-        System.out.println(input);
-        String encrypted = CesarEncryptionAttack.encrypt(input);
-        System.out.println(encrypted);
-        String decrypted = CesarEncryptionAttack.decrypt(encrypted);
-        System.out.println(decrypted);
+        String input = inputFromUser();
+        System.out.println("originalText: " + input);
+
+        int minDifference = Integer.MAX_VALUE;
+        int foundKey = 0;
+        String decryptedText = "";
+
+        for (int key = 1; key <= 26; key++) {
+            String decrypted = decrypt(input, key);
+            int difference = analyzeFrequency(decrypted);
+
+            if (difference < minDifference) {
+                minDifference = difference;
+                foundKey = key;
+                decryptedText = decrypted;
+            }
+        }
+
+        System.out.println("Key Found: " + foundKey);
+        System.out.println("Decrypted Text: " + decryptedText);
     }
 
     public static String inputFromUser() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your text to encrypt: ");
+        System.out.println("Enter your text to decrypt: ");
         String result = sc.nextLine();
         sc.close();
         return result;
     }
 
-    public static String encrypt(String message) {
+    public static String decrypt(String message, int key) {
         StringBuilder sb = new StringBuilder(message);
-        int distance = 5;
         for (int i = 0; i < sb.length(); i++) {
-            int c = (int)sb.charAt(i);
-            if (c + distance > 122) {
-                c = 31 + (distance - (122 - c));
+            char c = sb.charAt(i);
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                c = (char) ((((c - base - key + 26) % 26) + base));
+                sb.setCharAt(i, c);
             }
-            else {
-                c += distance;
-            }
-            sb.setCharAt(i, (char)c);
         }
         return sb.toString();
     }
 
-    public static String decrypt(String message) {
-        StringBuilder sb = new StringBuilder(message);
-        int distance = 5;
-        for (int i = 0; i < sb.length(); i++) {
-            int c = (int)sb.charAt(i);
-            if (c - distance < 32) {
-                c = 123 - (distance - (c - 32));
+    public static int analyzeFrequency(String text) {
+        double[] expectedFrequency = {
+                8.17, 1.49, 2.78, 4.25, 12.70, 2.23, 2.02, 6.09,
+                6.97, 0.15, 0.77, 4.03, 2.41, 6.75, 7.51, 1.93,
+                0.09, 5.99, 6.33, 9.06, 2.76, 0.98, 2.36, 0.15,
+                1.98, 0.07
+        };
+
+        int totalDifference = 0;
+
+        int[] actualFrequency = new int[26];
+        text = text.toLowerCase();
+
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                int index = c - 'a';
+                actualFrequency[index]++;
             }
-            else {
-                c -= distance;
-            }
-            sb.setCharAt(i, (char)c);
         }
-        return sb.toString();
-    }
 
-    public static void dict(String[] args) {
-        Map<Character, Integer> dictionary = new HashMap<>();
+        for (int i = 0; i < 26; i++) {
+            int difference = Math.abs(actualFrequency[i] - (int) (text.length() * (expectedFrequency[i] / 100.0)));
+            totalDifference += difference;
+        }
 
-        dictionary.put('A', 6);
-        dictionary.put('B', 35);
-        dictionary.put('C', 30);
-        dictionary.put('D', 21);
-        dictionary.put('F', 31);
-        dictionary.put('G', 38);
-        dictionary.put('H', 16);
-        dictionary.put('I', 1);
-        dictionary.put('J', 61);
-        dictionary.put('K', 53);
-        dictionary.put('L', 16);
-        dictionary.put('M', 26);
-        dictionary.put('N', 7);
-        dictionary.put('O', 3);
-        dictionary.put('P', 33);
-        dictionary.put('Q', 61);
-        dictionary.put('R', 10);
-        dictionary.put('S', 8);
-        dictionary.put('U', 21);
-        dictionary.put('V', 37);
-        dictionary.put('W', 23);
-        dictionary.put('X', 67);
-        dictionary.put('Y', 36);
-        dictionary.put('Z', 80);
+        return totalDifference;
     }
 }
